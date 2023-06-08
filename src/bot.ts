75915f5c7f43e8b51f4efb500cd9e6d5ed629e8c
czl531 <82907627+czl531@users.noreply.github.com>
2023-06-09 05:37:20 +0800

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -337,12 +337,14 @@ export class ChatGPTBot {
 	})
       return;
     }else if (rawText === '好无聊啊') {
-	await fetch('https://api.qqsuu.cn/api/dm-xjj?type=json')
+	await fetch('https://v.api.aa1.cn/api/api-girl-11-02/index.php?type=json')
 	    .then(response =>{
     		return response.json();
 	}).then(json=>{
-    		console.log(json.video)
-	    	message.say(FileBox.fromUrl(json.video))
+		//let encode_url = encodeURI(url1)
+		const mm = 'https:'+json.mp4
+		console.log(mm)
+	    	message.say(FileBox.fromUrl(mm))
 	})
       return;
     } else if (rawText === '来点疯狂星期四') {
